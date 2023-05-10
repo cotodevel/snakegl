9a1ab282307045efd1e59d67f61dd3267cd60787
Coto88 <programmer.coto.1988@gmail.com>
2023-05-10 00:16:41 -0400

    - Implement glGenLists() to be called multiple times, as the OpenGL (1.0) standard says. 	As a side effect, implementing this restores OpenGL standard calls (removing USERSPACE_TGDS_OGL_DL_POINTER and INTERNAL_TGDS_OGL_DL_POINTER), thus, removing useless duplicate OGL code 	across TGDS projects, restoring maintainability.

--- arm9/source/snake-src/src/snake.cpp
+++ arm9/source/snake-src/src/snake.cpp
@@ -58,50 +58,37 @@ void Snake::draw()
     // Use glut2Cylinder. 
     Point h = points[0];
 
-	#ifdef WIN32
-    glColor3f(1.0, 1.0, 0.6);
+	glColor3f(1.0, 1.0, 0.6);
     glPushMatrix();
         glTranslatef(h.x, h.y, h.z);
-	#endif
-
-	#ifdef ARM9
-    glColor3f(1.0, 1.0, 0.6, USERSPACE_TGDS_OGL_DL_POINTER);
-    glPushMatrix(USERSPACE_TGDS_OGL_DL_POINTER);
-        glTranslatef(h.x, h.y, h.z, USERSPACE_TGDS_OGL_DL_POINTER);
-	#endif
-        glut2SolidCube05f();
-	#ifdef WIN32
-    glPopMatrix();
-	#endif
-	#ifdef ARM9
-	glPopMatrix(1, USERSPACE_TGDS_OGL_DL_POINTER);
-    #endif
+	    glut2SolidCube05f();
+	glPopMatrix(
+		#ifdef ARM9
+		1
+		#endif
+	);
     enable_2D_texture();
-	#ifdef WIN32
-    glBindTexture(GL_TEXTURE_2D, texturesSnakeGL[SNAKE_TEXTURE]);
-	#endif
-	#ifdef ARM9
-	glBindTexture(0, texturesSnakeGL[SNAKE_TEXTURE], USERSPACE_TGDS_OGL_DL_POINTER);
-	#endif
-    for (size_t i = 1; i < points.size(); ++i)
+	glBindTexture(
+		#ifdef WIN32
+		GL_TEXTURE_2D,
+		#endif
+		#ifdef ARM9
+		0, 
+		#endif
+	texturesSnakeGL[SNAKE_TEXTURE]);
+	
+	for (size_t i = 1; i < points.size(); ++i)
     {
         Point p = points.at(i);
-		#ifdef WIN32
-        glPushMatrix();
-            glTranslatef(p.x, p.y, p.z);
-		#endif
 
-		#ifdef ARM9
-        glPushMatrix(USERSPACE_TGDS_OGL_DL_POINTER);
-            glTranslatef(p.x, p.y, p.z, USERSPACE_TGDS_OGL_DL_POINTER);
-		#endif
-            glut2SolidCube05f();
-		#ifdef WIN32
-        glPopMatrix();
-		#endif
-		#ifdef ARM9
-		glPopMatrix(1, USERSPACE_TGDS_OGL_DL_POINTER);
-		#endif
+		glPushMatrix();
+            glTranslatef(p.x, p.y, p.z);
+		    glut2SolidCube05f();
+		glPopMatrix(
+			#ifdef ARM9
+			1
+			#endif
+		);
     }
 
     disable_2D_texture();
@@ -220,167 +207,92 @@ __attribute__((optnone))
 void setupDLEnableDisable2DTextures(){
 	// Generate Different Lists
 	// 1: enable_2D_texture()
-	#ifdef WIN32
-	DLEN2DTEX=(GLint)glGenLists(4);							
+	DLEN2DTEX=(GLint)glGenLists(1);							
 	glNewList(DLEN2DTEX,GL_COMPILE);						
-	#endif
-	#ifdef ARM9
-	DLEN2DTEX=(GLint)glGenLists(4, USERSPACE_TGDS_OGL_DL_POINTER);						
-	glNewList(DLEN2DTEX,GL_COMPILE, USERSPACE_TGDS_OGL_DL_POINTER);
-	#endif
 	{
-		#ifdef WIN32
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		#endif
-        
-		#ifdef ARM9
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT, USERSPACE_TGDS_OGL_DL_POINTER);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT, USERSPACE_TGDS_OGL_DL_POINTER);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR, USERSPACE_TGDS_OGL_DL_POINTER);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR, USERSPACE_TGDS_OGL_DL_POINTER);
-		#endif
-        
+	   
 		GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f }; 
 		GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f }; 
 		GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f }; 
 		GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f }; 
 		
-		#ifdef WIN32
-        glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient); //WIN32
-        glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse); //WIN32
-        glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular); //WIN32
-        glLightfv(GL_LIGHT0, GL_POSITION, light_position); //WIN32
+		glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient); 
+        glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse); 
+        glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular); 
+        glLightfv(GL_LIGHT0, GL_POSITION, light_position); 
+		
 		
 		{
+			#ifdef WIN32
 			GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f }; //WIN32
 			GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f }; //WIN32
 			GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f }; //WIN32
 			GLfloat high_shininess[] = { 0.0f }; //WIN32
-			
-			glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT,   mat_ambient); //WIN32
-			glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,   mat_diffuse); //WIN32
-			glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR,  mat_specular); //WIN32
-			glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, high_shininess); //WIN32
-		}
-		#endif
-
-		#ifdef ARM9
-        glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient, USERSPACE_TGDS_OGL_DL_POINTER); //NDS
-        glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse, USERSPACE_TGDS_OGL_DL_POINTER); //NDS
-        glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular, USERSPACE_TGDS_OGL_DL_POINTER); //NDS
-        glLightfv(GL_LIGHT0, GL_POSITION, light_position, USERSPACE_TGDS_OGL_DL_POINTER); //NDS
-		
-		{
+			#endif
+			#ifdef ARM9
 			GLfloat mat_ambient[]    = { 60.0f, 60.0f, 60.0f, 60.0f }; //NDS
 			GLfloat mat_diffuse[]    = { 1.0f, 1.0f, 1.0f, 1.0f }; //NDS
 			GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f }; //NDS
 			GLfloat high_shininess[] = { 128.0f }; //NDS
-			
-			glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT,   mat_ambient, USERSPACE_TGDS_OGL_DL_POINTER); //NDS
-			glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,   mat_diffuse, USERSPACE_TGDS_OGL_DL_POINTER); //NDS
-			glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR,  mat_specular, USERSPACE_TGDS_OGL_DL_POINTER); //NDS
-			glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, high_shininess, USERSPACE_TGDS_OGL_DL_POINTER); //NDS
+			#endif
+			glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT,   mat_ambient);
+			glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,   mat_diffuse); 
+			glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR,  mat_specular); 
+			glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, high_shininess);
 		}
-		#endif
 	}
 	
-	#ifdef WIN32
 	glEndList();
-	#endif
-	
-	#ifdef ARM9
-	glEndList(USERSPACE_TGDS_OGL_DL_POINTER);
-	#endif
-	
-	DLDIS2DTEX=DLEN2DTEX+1;
+	DLDIS2DTEX=glGenLists(1);
 
 	// 2: disable_2D_texture()
-	#ifdef ARM9
-	glNewList(DLDIS2DTEX,GL_COMPILE, USERSPACE_TGDS_OGL_DL_POINTER);							
-	#endif
-	#ifdef WIN32
-	glNewList(DLDIS2DTEX,GL_COMPILE);							
-	#endif
+	glNewList(DLDIS2DTEX,GL_COMPILE);
 	{
-		#ifdef ARM9
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT, USERSPACE_TGDS_OGL_DL_POINTER);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT, USERSPACE_TGDS_OGL_DL_POINTER);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR, USERSPACE_TGDS_OGL_DL_POINTER);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR, USERSPACE_TGDS_OGL_DL_POINTER);
-		#endif
-        
-		#ifdef WIN32
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		#endif
-        
+		
 		GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
         GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
         GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
         GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
 
-		#ifdef ARM9
-        glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient, USERSPACE_TGDS_OGL_DL_POINTER); //NDS
-        glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse, USERSPACE_TGDS_OGL_DL_POINTER); //NDS
-        glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular, USERSPACE_TGDS_OGL_DL_POINTER); //NDS
-        glLightfv(GL_LIGHT0, GL_POSITION, light_position, USERSPACE_TGDS_OGL_DL_POINTER); //NDS
+		glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient); 
+        glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse); 
+        glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular); 
+        glLightfv(GL_LIGHT0, GL_POSITION, light_position); 
+
 		
 		{
+			#ifdef ARM9
 			GLfloat mat_ambient[]    = { 60.0f, 60.0f, 60.0f, 60.0f }; //NDS
 			GLfloat mat_diffuse[]    = { 1.0f, 1.0f, 1.0f, 1.0f }; //NDS
 			GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f }; //NDS
 			GLfloat high_shininess[] = { 128.0f }; //NDS
-			
-			glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT,   mat_ambient, USERSPACE_TGDS_OGL_DL_POINTER); //NDS
-			glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,   mat_diffuse, USERSPACE_TGDS_OGL_DL_POINTER); //NDS
-			glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR,  mat_specular, USERSPACE_TGDS_OGL_DL_POINTER); //NDS
-			glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, high_shininess, USERSPACE_TGDS_OGL_DL_POINTER); //NDS
-		}
-		#endif
-        
-		#ifdef WIN32
-        glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient); //WIN32
-        glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse); //WIN32
-        glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular); //WIN32
-        glLightfv(GL_LIGHT0, GL_POSITION, light_position); //WIN32
-		
-		{
+			#endif
+
+			#ifdef WIN32
 			GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f }; //WIN32
 			GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f }; //WIN32
 			GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f }; //WIN32
 			GLfloat high_shininess[] = { 100.0f }; //WIN32
-			
-			glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT,   mat_ambient); //WIN32
-			glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,   mat_diffuse); //WIN32
-			glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR,  mat_specular); //WIN32
-			glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, high_shininess); //WIN32
+			#endif
+			glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT,   mat_ambient); 
+			glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,   mat_diffuse);
+			glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR,  mat_specular);
+			glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, high_shininess);
 		}
-		#endif
-        
 	}
-	#ifdef ARM9
-	glEndList(USERSPACE_TGDS_OGL_DL_POINTER);
-	#endif
-
-	#ifdef WIN32
 	glEndList();
-	#endif
-
-	DLSOLIDCUBE05F=DLDIS2DTEX+1;
+	DLSOLIDCUBE05F=glGenLists(1);
 
 	// 3: draw solid cube: 0.5f()
-	#ifdef ARM9
-	glNewList(DLSOLIDCUBE05F,GL_COMPILE, USERSPACE_TGDS_OGL_DL_POINTER);							
-	#endif
-
-	#ifdef WIN32
-	glNewList(DLSOLIDCUBE05F,GL_COMPILE);							
-	#endif
+	glNewList(DLSOLIDCUBE05F, GL_COMPILE);
 	{
 		float size = 0.5f;
 		GLfloat n[6][3] =
@@ -413,7 +325,6 @@ void setupDLEnableDisable2DTextures(){
 
 		for (i = 5; i >= 0; i--)
 		{
-			#ifdef WIN32
 			glBegin(GL_QUADS);
 				glNormal3fv(&n[i][0]);
 				glTexCoord2f(0, 0);
@@ -425,38 +336,13 @@ void setupDLEnableDisable2DTextures(){
 				glTexCoord2f(0, 1);
 				glVertex3fv(&v[faces[i][3]][0]);
 			glEnd();
-			#endif
-			#ifdef ARM9
-			glBegin(GL_QUADS, USERSPACE_TGDS_OGL_DL_POINTER);
-				glNormal3fv(&n[i][0], USERSPACE_TGDS_OGL_DL_POINTER);
-				glTexCoord2f(0, 0, USERSPACE_TGDS_OGL_DL_POINTER);
-				glVertex3fv(&v[faces[i][0]][0], USERSPACE_TGDS_OGL_DL_POINTER);
-				glTexCoord2f(1, 0, USERSPACE_TGDS_OGL_DL_POINTER);
-				glVertex3fv(&v[faces[i][1]][0], USERSPACE_TGDS_OGL_DL_POINTER);
-				glTexCoord2f(1, 1, USERSPACE_TGDS_OGL_DL_POINTER);
-				glVertex3fv(&v[faces[i][2]][0], USERSPACE_TGDS_OGL_DL_POINTER);
-				glTexCoord2f(0, 1, USERSPACE_TGDS_OGL_DL_POINTER);
-				glVertex3fv(&v[faces[i][3]][0], USERSPACE_TGDS_OGL_DL_POINTER);
-			glEnd(USERSPACE_TGDS_OGL_DL_POINTER);
-			#endif
 		}
 	}
 
-	#ifdef WIN32
 	glEndList();
-	#endif
-	#ifdef ARM9
-	glEndList(USERSPACE_TGDS_OGL_DL_POINTER);
-	#endif
-	
-	DLSPHERE=DLSOLIDCUBE05F+1;
+	DLSPHERE=glGenLists(1);
 
-	#ifdef WIN32
-	glNewList(DLSPHERE,GL_COMPILE);							// 4: draw sphere
-	#endif
-	#ifdef ARM9
-	glNewList(DLSPHERE,GL_COMPILE, USERSPACE_TGDS_OGL_DL_POINTER);							// 4: draw sphere
-	#endif
+	glNewList(DLSPHERE, GL_COMPILE);	// 4: draw sphere
 	{
 		#define TWOPI ((double)((3.141592f)*2))
 		double r=0.6,faces=12;
@@ -477,13 +363,7 @@ void setupDLEnableDisable2DTextures(){
 			theta1 = j * TWOPI / faces - PID2;
 			theta2 = (j + 1) * TWOPI / faces - PID2;
 
-			#ifdef WIN32
 			glBegin(GL_TRIANGLE_STRIP);
-			#endif
-			
-			#ifdef ARM9
-			glBegin(GL_TRIANGLE_STRIP, USERSPACE_TGDS_OGL_DL_POINTER);
-			#endif
 			
 			for (i=0;i<=faces;i++) {
 				theta3 = i * TWOPI / faces;
@@ -494,22 +374,11 @@ void setupDLEnableDisable2DTextures(){
 				py = cy + r * ey;
 				pz = cz + r * ez;
 				
-				#ifdef ARM9
-				glNormal3f(ex,ey,ez, USERSPACE_TGDS_OGL_DL_POINTER);
-				#endif
-				#ifdef WIN32
 				glNormal3f(ex,ey,ez);
-				#endif
-
-				#ifdef ARM9
-				glTexCoord2f(-i/(double)faces,2*(j+1)/(double)faces, USERSPACE_TGDS_OGL_DL_POINTER);
-				glVertex3f(px,py,pz, USERSPACE_TGDS_OGL_DL_POINTER);
-				#endif
-
-				#ifdef WIN32
+				
 				glTexCoord2f(-i/(double)faces,2*(j+1)/(double)faces);
 				glVertex3f(px,py,pz);
-				#endif
+				
 				ex = cos(theta1) * cos(theta3);
 				ey = sin(theta1);
 				ez = cos(theta1) * sin(theta3);
@@ -517,49 +386,26 @@ void setupDLEnableDisable2DTextures(){
 				py = cy + r * ey;
 				pz = cz + r * ez;
 
-				#ifdef WIN32
 				glNormal3f(ex,ey,ez);
 				glTexCoord2f(-i/(double)faces,2*j/(double)faces);
 				glVertex3f(px,py,pz);
-				#endif
-				#ifdef ARM9
-				glNormal3f(ex,ey,ez, USERSPACE_TGDS_OGL_DL_POINTER);
-				glTexCoord2f(-i/(double)faces,2*j/(double)faces, USERSPACE_TGDS_OGL_DL_POINTER);
-				glVertex3f(px,py,pz, USERSPACE_TGDS_OGL_DL_POINTER);
-				#endif
 			}
-			#ifdef WIN32
 			glEnd();
-			#endif
-			#ifdef ARM9
-			glEnd(USERSPACE_TGDS_OGL_DL_POINTER);
-			#endif
 		}
 	}
-	#ifdef WIN32
 	glEndList();
-	#endif
-	#ifdef ARM9
-	glEndList(USERSPACE_TGDS_OGL_DL_POINTER);
-	#endif
 }
 
 void enable_2D_texture(){
-	#ifdef ARM9
-	glCallList(DLEN2DTEX, USERSPACE_TGDS_OGL_DL_POINTER);
-	#endif
-	#ifdef WIN32
 	glCallList(DLEN2DTEX);
+	#ifdef WIN32
 	glEnable(GL_TEXTURE_2D); //win32 only
 	#endif
 }
 
 void disable_2D_texture(){
-	#ifdef ARM9
-	glCallList(DLDIS2DTEX, USERSPACE_TGDS_OGL_DL_POINTER);
-	#endif
-	#ifdef WIN32
 	glCallList(DLDIS2DTEX);
+	#ifdef WIN32
 	glDisable(GL_TEXTURE_2D); //win32 only
 	#endif
 }
@@ -567,28 +413,24 @@ void disable_2D_texture(){
 void draw_cube(float size, Point p, int res_id)
 {
     enable_2D_texture();
-	#ifdef WIN32
-    glPushMatrix();
-	#endif
-	#ifdef ARM9
-    glPushMatrix(USERSPACE_TGDS_OGL_DL_POINTER);
-	#endif
-		#ifdef ARM9
-        glBindTexture(0, texturesSnakeGL[res_id], USERSPACE_TGDS_OGL_DL_POINTER);
-		glTranslatef(p.x, p.y, p.z, USERSPACE_TGDS_OGL_DL_POINTER);		
-		#endif
-		#ifdef WIN32
-		glBindTexture(GL_TEXTURE_2D, texturesSnakeGL[res_id]);
+	glPushMatrix();
+		glBindTexture(
+			#ifdef ARM9
+			0, 
+			#endif
+			#ifdef WIN32
+			GL_TEXTURE_2D,
+			#endif
+			texturesSnakeGL[res_id]
+		);
 		glTranslatef(p.x, p.y, p.z);
-		#endif
 		glut2SolidCube(size);
-	#ifdef ARM9
-    glPopMatrix(1, USERSPACE_TGDS_OGL_DL_POINTER);
-	#endif
-	#ifdef WIN32
-    glPopMatrix();
-	#endif
-    disable_2D_texture();
+	glPopMatrix(
+		#ifdef ARM9
+		1
+		#endif
+	);
+	disable_2D_texture();
 }
 
 void drawSphere(){
@@ -636,7 +478,7 @@ void unload_resources()
 //Todo DS: use WoopsiSDK instead
 void draw_text(string s, Point p, float r, float g, float b)
 {
-    glDisable(GL_LIGHTING, USERSPACE_TGDS_OGL_DL_POINTER);
+    glDisable(GL_LIGHTING);
 
 	#ifdef WIN32
     int len, i;
@@ -657,20 +499,14 @@ void draw_text(string s, Point p, float r, float g, float b)
     nocashMessage(debugBuf);
     #endif
 	
-	glEnable(GL_LIGHTING, USERSPACE_TGDS_OGL_DL_POINTER);
+	glEnable(GL_LIGHTING);
 }
 
 //hardcoded below function at 0.5f to speedup drawing
 void glut2SolidCube05f(){
-	#ifdef ARM9
-    //(snake body object)
-	glColor3f(1.0f, 1.0f, 14.0f, USERSPACE_TGDS_OGL_DL_POINTER);
-	glCallList(DLSOLIDCUBE05F, USERSPACE_TGDS_OGL_DL_POINTER);
-	#endif
-	#ifdef WIN32
+	//(snake body object)
 	glColor3f(1.0f, 1.0f, 14.0f);
 	glCallList(DLSOLIDCUBE05F);
-	#endif
 }
 
 void glut2SolidCube(GLdouble size){
@@ -704,23 +540,8 @@ void glut2SolidCube(GLdouble size){
 
     for (i = 5; i >= 0; i--)
     {
-		#ifdef ARM9
-        glBegin(GL_QUADS, USERSPACE_TGDS_OGL_DL_POINTER);
+		glBegin(GL_QUADS);
 			//(snake head object)
-			glColor3f(1.0f, 1.0f, 14.0f, USERSPACE_TGDS_OGL_DL_POINTER);
-            glNormal3fv(&n[i][0], USERSPACE_TGDS_OGL_DL_POINTER);
-            glTexCoord2f(0, 0, USERSPACE_TGDS_OGL_DL_POINTER);
-			glVertex3fv(&v[faces[i][0]][0], USERSPACE_TGDS_OGL_DL_POINTER);
-            glTexCoord2f(1, 0, USERSPACE_TGDS_OGL_DL_POINTER);
-            glVertex3fv(&v[faces[i][1]][0], USERSPACE_TGDS_OGL_DL_POINTER);
-            glTexCoord2f(1, 1, USERSPACE_TGDS_OGL_DL_POINTER);
-            glVertex3fv(&v[faces[i][2]][0], USERSPACE_TGDS_OGL_DL_POINTER);
-            glTexCoord2f(0, 1, USERSPACE_TGDS_OGL_DL_POINTER);
-            glVertex3fv(&v[faces[i][3]][0], USERSPACE_TGDS_OGL_DL_POINTER);
-        glEnd(USERSPACE_TGDS_OGL_DL_POINTER);
-		#endif
-		#ifdef WIN32
-        glBegin(GL_QUADS);
 			glColor3f(1.0f, 1.0f, 14.0f);
             glNormal3fv(&n[i][0]);
             glTexCoord2f(0, 0);
@@ -732,7 +553,6 @@ void glut2SolidCube(GLdouble size){
             glTexCoord2f(0, 1);
             glVertex3fv(&v[faces[i][3]][0]);
         glEnd();
-		#endif
     }
 }
 
